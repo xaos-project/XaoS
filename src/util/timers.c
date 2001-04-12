f0fe88d017041d8f4a745a658c1ea475f36dd9eb
hubicka <hubicka@448e8058-b851-0410-8f1a-96ed746e2993>
2001-04-12 22:26:53 +0000

    Win32 fix

--- src/util/timers.c
+++ src/util/timers.c
@@ -72,7 +72,12 @@
 #endif
 
 #ifdef _WIN32
-#define QuadPart(x) (((long long)x.HighPart << 32) + x.LowPart)
+/*#define QuadPart(x) (((long long)x.HighPart << 32) + x.LowPart)*/
+#ifndef _ANONYMOUS_STRUCT
+#define QuadPart(x) x.u.QuadPart
+#else
+#define QuadPart(x) x.QuadPart
+#endif
 #endif
 
 #ifndef SIGALRM
