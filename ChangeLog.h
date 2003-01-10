93466275c27b9c0857628198a189b82a46f306c2
kovzol <kovzol@448e8058-b851-0410-8f1a-96ed746e2993>
2003-01-10 11:00:47 +0000

    ChangeLog.[ch] was indented, they should not! Fixing do-indent.

--- ChangeLog.h
+++ ChangeLog.h
@@ -1,10 +1,11 @@
 o Line drawing functions - not very fast,
-  but performs antialiasing etc..o Enabled forgotten optimizations for bpp ==
-  1 o Better I / B / P frame decision
-  algorithm (now seems to give quite good results)
-     o Motion vector guessing ...
-       o Fixed some bugs in antialiasing,
-       vertical /
-       horizontal line drawing and such o Some new options for rendering o
-       Document how to generate seqences for MPEG encoding o Fixed command
-       flags - now works commands as loadpos
+  but performs antialiasing etc.
+o Enabled forgotten optimizations for bpp == 1
+o Better I / B / P frame decision algorithm
+  (now seems to give quite good results)
+o Motion vector guessing ...
+o Fixed some bugs in antialiasing, vertical /
+  horizontal line drawing and such
+o Some new options for rendering
+o Document how to generate seqences for MPEG encoding
+o Fixed command flags - now works commands as loadpos
