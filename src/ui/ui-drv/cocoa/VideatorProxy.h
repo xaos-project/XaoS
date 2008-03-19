5a9b5c17a8e43d00d2ad9ea26b57cde4411444a2
jblang <jblang@448e8058-b851-0410-8f1a-96ed746e2993>
2008-03-19 05:02:12 +0000

    Oops--forgot to add VideatorProxy before

---
+++ src/ui/ui-drv/cocoa/VideatorProxy.h
@@ -0,0 +1,38 @@
+/*
+ *     XaoS, a fast portable realtime fractal zoomer 
+ *                  Copyright (C) 1996 by
+ *
+ *      Jan Hubicka          (hubicka@paru.cas.cz)
+ *      Thomas Marsh         (tmarsh@austin.ibm.com)
+ *
+ *    Cocoa Driver by J.B. Langston III (jb-langston@austin.rr.com)
+ *    Videator Support by Andrew Stone (Stone Design)
+ *    For information about Videator, visit http://www.stone.com/Videator
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#import <Cocoa/Cocoa.h>
+
+
+@interface VideatorProxy : NSObject {
+	BOOL _wantsThrottle;
+	NSDate *_lastFrameCreatedDate;
+	id _videatorProxy;
+	NSCalendarDate *_killDate;
+}
+
+- (void)sendImageRep:(NSBitmapImageRep *)imageRep;
+
+@end
