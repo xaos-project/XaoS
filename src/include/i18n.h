4b2631926d4ab22ad4a9a69ac587e114af5df190
J.B. Langston <jb.langston@gmail.com>
2019-05-09 04:42:38 -0500

    Fix compile error when gettext isn't present

--- src/include/i18n.h
+++ src/include/i18n.h
@@ -2,25 +2,24 @@
 #define I18N_H
 #include "aconfig.h"
 
-#if defined(HAVE_GETTEXT)
+#ifdef HAVE_GETTEXT
 // Use gettext library
 #include <libintl.h>
 #include <locale.h>
-
-#elif defined (QT_GETTEXT)
-// Emulate gettext call using Qt's translation framework
+#else
+// No-op textdomain functions
 #define bindtextdomain(x, y) (y)
 #define textdomain(x)
+#ifdef QT_GETTEXT
+// Emulate gettext call using Qt's translation framework
 const char *qt_gettext(const char *text);
 const char *qt_locale();
 #define gettext(STRING) qt_gettext(STRING)
-
 #else
-// No I18N available, just pass original string
+// No I18N available, no-op gettext function
 #define gettext(STRING) STRING
 #endif
-
+#endif
 // Define _ as alias for gettext
 #define _(STRING) gettext(STRING)
-
 #endif
