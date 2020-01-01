b30cb0ef3b7a9c3d4cde9e9d32b7536a2f48b6dc
Zoltán Kovács <zoltan@geogebra.org>
2020-01-01 16:16:48 +0100

    Add more info on usage

--- tools/nametest.sh
+++ tools/nametest.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # This scripts decides if there are same names in different directories.
 # If yes, we should consider renaming one of the files.
+# Use this tool from the folder "examples".
 find -name '*.xpf' -printf "%f\n" | sort | uniq -c | grep -v " 1 "
