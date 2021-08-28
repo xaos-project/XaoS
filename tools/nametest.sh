ff50406cea5a5b9ffbd2a9d732b397182b0ca086
a1346054 <36859588+a1346054@users.noreply.github.com>
2021-08-28 13:58:13 +0000

    Fix shellcheck issues
    Mainly fixes the case where the working directory path contains a space,
    and also makes the code more robust and portable.

--- tools/nametest.sh
+++ tools/nametest.sh
@@ -2,4 +2,4 @@
 # This scripts decides if there are same names in different directories.
 # If yes, we should consider renaming one of the files.
 # Use this tool from the folder "examples".
-find -name '*.xpf' -printf "%f\n" | sort | uniq -c | grep -v " 1 "
+find . -name '*.xpf' -printf "%f\n" | sort | uniq -c | grep -v " 1 "
